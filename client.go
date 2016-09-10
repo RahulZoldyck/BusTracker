30289b7023f84bf1c76195a99b0b03bff058591e
Rahul Zoldyck <rahulzoldyck@gmail.com>
2016-09-11 03:49:15 +0530

    Initial Commit
    Initialized wireframe for the project

---
+++ client.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+"github.com/gorilla/websocket"
+"time"
+)
+
+const (
+// Time allowed to write a message to the peer.
+	writeWait = 10 * time.Second
+
+// Time allowed to read the next pong message from the peer.
+	pongWait = 60 * time.Second
+
+// Send pings to peer with this period. Must be less than pongWait.
+	pingPeriod = (pongWait * 9) / 10
+
+// Maximum message size allowed from peer.
+	maxMessageSize = 512
+)
+
+var (
+	newline = []byte{'\n'}
+	space   = []byte{' '}
+)
+
+type Client struct {
+	hub *Hub
+
+	// The websocket connection.
+	conn *websocket.Conn
+
+	// Buffered channel of outbound messages.
+	send chan []byte
+}
+
+
